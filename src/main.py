b4127a7f948fabdf82cbca4013dfb870f87c0cd0
Richard Shelmerdine <richard@richardshelmerdine.com>
2025-10-02 21:42:12 +0100

    fixing issue with import block and lumber not accessible

--- src/main.py
+++ src/main.py
@@ -10,6 +10,7 @@ import pandas as pd
 import yfinance as yf
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+from datetime import datetime
 
 
 # ---------------- Config / IO ----------------
